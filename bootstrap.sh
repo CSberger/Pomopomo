aceb884b9d7d31eab07f20d6723d14ed1aa755ce
CSBerger <christopher.s.berger@gmail.com>
2014-07-25 23:25:07 -0700

    python3.4 django provisioning

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,13 @@ add-apt-repository ppa:fkrull/deadsnakes
 apt-get update
 apt-get install python3.4 -y
 apt-get install curl -y
-#curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
-#sudo python3.4 get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
+python3.4 get-pip.py
+python3.4 -m venv ~/venv
+source ~/venv/activate
+pip3.4 install -r /vagrant/pip-requirements.txt
+pip3.4 install https://www.djangoproject.com/download/1.7c1/tarball/
+deactivate
+
 rm -rf /var/www
 ln -fs /vagrant /var/www
