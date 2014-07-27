db3625c9c2d71d74d78e2d8da9a4de81f687f734
Chris Berger <contact@christopherberger.info>
2014-07-27 14:25:44 -0700

    new app started with django 1.7

--- Pomopomo/settings.py
+++ Pomopomo/settings.py
@@ -1,11 +1,11 @@
 """
-Django settings for Pomopomo project.
+Django settings for pomopomo project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
+https://docs.djangoproject.com/en/dev/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
+https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -14,10 +14,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w4i-ar74*ga@g=78ls2j^+pvu42kqg0@%pz(e11+)7t^mh^z^3'
+SECRET_KEY = '$&s%q1%*ez&h(o2nq+y+5040yin)&p(w7+$-%4^#93b$#86-hf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pomotask',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -44,27 +43,42 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'Pomopomo.urls'
+ROOT_URLCONF = 'pomopomo.urls'
 
-WSGI_APPLICATION = 'Pomopomo.wsgi.application'
+WSGI_APPLICATION = 'pomopomo.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DB_MAP = {
+    'dev': dict(name='django-pomo', user='postgres', password=os.getenv('DB_PASS'), host='localhost', port='5432'),
+    'test': {},
+    'prod': {}
+}
 
+current_db = DB_MAP['dev']
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': current_db['name'],
+        'USER': current_db['user'],
+        'PASSWORD': current_db['password'],
+        'HOST': current_db['host']
     }
 }
 
+
 # Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+# https://docs.djangoproject.com/en/dev/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -78,10 +92,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
-)
--- Pomopomo/wsgi.py
+++ Pomopomo/wsgi.py
@@ -1,14 +1,14 @@
 """
-WSGI config for Pomopomo project.
+WSGI config for pomopomo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Pomopomo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pomopomo.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
